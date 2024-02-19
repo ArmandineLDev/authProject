056e3050605a62ce710031d27c6dbcad18da8d9b
Armandine Barge <armandine.dev@gmail.com>
2024-02-19 14:43:42 +0100

    prisma conf + create user + middleware loggin

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,9 +2,12 @@
 
 "use server";
 
+import bcryp from "bcrypt";
 import { z } from "zod";
 
 import { RegisterSchema } from "@/src/schemas";
+import { db } from "@/src/lib/db";
+import { getUserByEmail } from "@/src/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -13,5 +16,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcryp.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) return { error: "User already exists" };
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO send verification token email
+
+  return { success: "Utilisateur créé" };
 };
