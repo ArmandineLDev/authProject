1a84474e0cecb30e30987a6049c6cc9c4e4b6659
Armandine Barge <armandine.dev@gmail.com>
2024-03-06 17:19:35 +0100

    verifying token with email

--- components/auth/NewVerificationForm.tsx
+++ components/auth/NewVerificationForm.tsx
@@ -1,8 +1,42 @@
 "use client";
-import { CircleLoader } from "react-spinners";
+
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+
+import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/CardWrapper";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (success || error) return;
+
+    if (!token) {
+      setError("Missing Token");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong");
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
       headerLabel="Confirmation de votre email"
@@ -10,7 +44,9 @@ export const NewVerificationForm = () => {
       backButtonHref="/auth/login"
     >
       <div className="flex items-center w-full justify-center">
-        <CircleLoader />
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
       </div>
     </CardWrapper>
   );
